81fdd19c79a5cfb1e83df9afd7cb6509c1b045e5
KanonKC <kanonkce@gmail.com>
2021-11-21 19:21:14 +0700

    Beautify Code

--- Solutions/Mini-Max Sum.py
+++ Solutions/Mini-Max Sum.py
@@ -1,17 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'miniMaxSum' function below.
-#
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-
 def miniMaxSum(arr):
     max_value = 0
     min_value = arr[0]
@@ -23,9 +9,3 @@ def miniMaxSum(arr):
         if arr[i] < min_value:
             min_value = arr[i]
     print(f"{res-max_value} {res-min_value}")
-
-if __name__ == '__main__':
-
-    arr = list(map(int, input().rstrip().split()))
-
-    miniMaxSum(arr)
